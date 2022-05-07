229003b29ce28fed07240c99f7aa6d05d831c6e0
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-05-07 07:18:02 +0700

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "10605524").strip()
+API_HASH = os.getenv("API_HASH", "a4ccc51331a895285a4c57ce10d49f95").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "5257644826:AAHUlM4qDhsitRE6O3z0Leg2J96hXj2QMzI").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "postgres://zzlrfwmp:75B5tfrr7ocO6MOsfogvSPn2XmZccKIF@chunee.db.elephantsql.com/zzlrfwmp").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "hiroshisupport")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
